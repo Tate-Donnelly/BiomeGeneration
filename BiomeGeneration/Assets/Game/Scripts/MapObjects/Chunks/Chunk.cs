a17b0e599d340fe4603a6cbdf5e32e0d0c2313e1
Tate <tate.donne@gmail.com>
2023-12-02 14:23:39 -0500

    Uses a int array to position tiles. Include's tile height, but not biome

--- BiomeGeneration/Assets/Game/Scripts/MapObjects/Chunks/Chunk.cs
+++ BiomeGeneration/Assets/Game/Scripts/MapObjects/Chunks/Chunk.cs
@@ -6,11 +6,27 @@ public class Chunk : MapObject
 {
     //0 is left of the chunk, 1 is above the chunk, 2 is to the right of the chunk, 3 is below the chunk
     private Chunk[] nearbyChunks = new Chunk[4];
+    private MapObject mapObject;
+    public int height;
 
-    public void Init(Chunk[] nearbyChunks, Mesh mesh, Material material)
+    public void Init(Chunk[] nearbyChunks, MapObject mapObject, Mesh mesh, Material material)
     {
         base.Init(mesh, material);
         this.nearbyChunks = nearbyChunks;
+        this.mapObject = mapObject;
+
+        InstantiateObject();
+    }
+
+    private void InstantiateObject()
+    {
+        if (mapObject != null)
+        {
+            Vector3 position = transform.position;
+            MapObject environmentObject =Instantiate(mapObject);
+            environmentObject.transform.position =
+                new Vector3(position.x, position.y + ChunkManager.tileHeight, position.z);
+        }
     }
     
 }
--- BiomeGeneration/Assets/Game/Scripts/MapObjects/Chunks/ChunkManager.cs
+++ BiomeGeneration/Assets/Game/Scripts/MapObjects/Chunks/ChunkManager.cs
@@ -2,17 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Manages loading the data and using it to create chunks with the specified position and biome.
+/// </summary>
 public class ChunkManager : MonoBehaviour
 {
-    [SerializeField] private Chunk chunk;
+    //Inspector
+    //References
+    [SerializeField] private Chunk chunkPrefab;
     [SerializeField] private Material _material;
     [SerializeField] private Mesh _mesh;
+    
+    //Variables
+    public static int tileWidth=2;
+    public static int tileHeight=2;
 
-    void Update()
+    //Internal
+    private int numRows;
+    private int numCols;
+    
+    private Chunk[,] chunks;
+
+    void Awake()
     {
-        if (Input.GetKey(KeyCode.A))
+        //TODO: Remove temporary data and LoadData function from Awake
+        int[,] data = { 
+            {1,1,1,1,1,2,3},
+            {1,1,1,1,1,2,2},
+            {1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,2},
+            {1,1,1,1,1,2,2},
+            {3,2,1,1,1,1,2}
+        };
+        LoadData(data);
+    }
+
+    public void LoadData(int[,] data)
+    {
+        numRows = data.GetLength(0);
+        numCols = data.GetLength(1);
+        print($"Rows:{numRows} Cols:{numCols}");
+        chunks=new Chunk[numRows,numCols];
+
+        SetChunks(data);
+        CreateChunks();
+    }
+
+    private void SetChunks(int[,] data)
+    {
+        for (int i = 0; i < numRows; i++)
         {
-            chunk.Init(_mesh,_material);
+            for (int j = 0; j < numCols; j++)
+            {
+                chunks[i,j] = new Chunk();
+                chunks[i, j].height = data[i, j];
+            }
+        }
+    }
+
+    private void CreateChunks()
+    {
+        float halfChunkWidth = tileWidth * numRows * .5f;
+        for (int i = 0; i < chunks.GetLength(0); i++)
+        {
+            for (int j = 0; j < chunks.GetLength(1); j++)
+            {
+                Chunk chunk=Instantiate(chunkPrefab, transform);
+                float xPos = ((i - halfChunkWidth) * tileWidth);
+                float zPos = ((j - halfChunkWidth) * tileWidth);
+                chunk.transform.position=new Vector3(xPos,chunks[i,j].height-1,zPos);
+            }
         }
     }
 }
