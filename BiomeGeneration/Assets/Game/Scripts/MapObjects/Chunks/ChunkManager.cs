e3a9bec5c60fd869b4ecb5237bfb81c4902bc35a
Tate <tate.donne@gmail.com>
2023-12-04 17:57:48 -0500

    Path orientation

--- BiomeGeneration/Assets/Game/Scripts/MapObjects/Chunks/ChunkManager.cs
+++ BiomeGeneration/Assets/Game/Scripts/MapObjects/Chunks/ChunkManager.cs
@@ -12,10 +12,7 @@ public class ChunkManager : MonoBehaviour
 {
     //Inspector
     //References
-    [SerializeField] private Chunk chunkPrefab;
-    [SerializeField] private Material _material;
-    [SerializeField] private Mesh _mesh;
-
+    [SerializeField] private Tile _tilePrefab;
     [SerializeField]
     private SerializedDictionary<TileMesh, Mesh> meshDictionary = new SerializedDictionary<TileMesh, Mesh>();
     [SerializeField]
@@ -28,7 +25,7 @@ public class ChunkManager : MonoBehaviour
     public static  int numCols;
 
     //Internal
-    private Chunk[,] chunks;
+    private Tile[,] chunks;
     private float mapWidth;
     private float mapLength;
 
@@ -37,10 +34,10 @@ public class ChunkManager : MonoBehaviour
         //TODO: Remove temporary chunkPositions and LoadData function from Awake
         string[,] chunk =
         {
-            { "1ca","1lh","1lh","1cb" },
-            { "1lv","0","1","1lv" },
+            { "1ca","1tt","1lh","1cb" },
+            { "1tr","1","1","1tl" },
             { "1lv","1","0","1lv" },
-            { "1cd","1lh","1tt","1cc" },
+            { "1cd","1td","1lh","1cc" },
         };
 
         int[,] biomes =
@@ -62,67 +59,87 @@ public class ChunkManager : MonoBehaviour
     }
 
     /// <summary>
-    /// Loads the mapPosition of a given chunk
+    /// Loads the mapPosition of a given tile
     /// </summary>
     /// <param name="mapPosition">An array where the chunks in the map</param>
     /// <param name="chunkPosition">An array containing the positioning of each
-    /// of the tiles in a chunk</param>
-    /// <param name="biome">The biome of the chunk</param>
+    /// of the tiles in a tile</param>
+    /// <param name="biome">The biome of the tile</param>
     public void LoadData(string[,] mapPosition, int[] chunkPosition, int biome)
     {
         numRows = mapPosition.GetLength(0);
         numCols = mapPosition.GetLength(1);
-        chunks=new Chunk[numRows,numCols];
+        chunks=new Tile[numRows,numCols];
         
         CreateChunks(mapPosition, chunkPosition,biome);
     }
 
     /// <summary>
-    /// Creates the tiles in the specified chunk
+    /// Creates the tiles in the specified tile
     /// </summary>
-    /// <param name="chunkPositions">The positions of tiles within the chunk</param>
+    /// <param name="chunkPositions">The positions of tiles within the tile</param>
     /// <param name="mapPosition">An array where the chunks in the map</param>
-    /// <param name="biome">The biome of the chunk</param>
+    /// <param name="biome">The biome of the tile</param>
     private void CreateChunks(string[,] chunkPositions,int[] mapPosition, int biome)
     {
         for (int i = 0; i < chunks.GetLength(0); i++)
         {
             for (int j = 0; j < chunks.GetLength(1); j++)
             {
-                print(chunkPositions[i,j][0]);
-                int height = chunkPositions[i, j][0];
+                string tileInfo=chunkPositions[i,j];
+                int height = tileInfo[0];
                 //Calculate the position
                 Vector3 pos = GetTilePosition(height,mapPosition,i,j);
                 
                 //Gets the correct biome material
                 Material material=materialDictionary[(Biome) biome];
                 
-                
-                //Create the actual chunk
-                Chunk chunk=Instantiate(chunkPrefab, transform);
                 //Gets the needed mesh
                 Mesh mesh = GetTileMesh(chunkPositions[i, j]);
-                
-                chunk.Init(pos,mesh,material);
+
+                //Create the actual tile
+                InstantiateTiles(chunkPositions[i, j],pos,mesh,material);
             }
         }
     }
 
     /// <summary>
-    /// Calculates the tile's position based on it's position in the chunk and the
-    /// chunk's position in the map
+    /// Uses the given info to instantiate the tile
     /// </summary>
-    /// <param name="chunkPositions">The array of positions within the chunk</param>
-    /// <param name="mapPosition">An array where the chunks in the map</param>
-    /// <param name="chunkRow">The row of the tile in chunkPositions</param>
-    /// <param name="chunkCol">The col of the tile in chunkPositions</param>
-    /// <returns>Returns the position of the tile in the scene</returns>
-    private Vector3 GetTilePosition(int height, int[] mapPosition, int chunkRow, int chunkCol)
+    /// <param name="tileInfo">The string of the title</param>
+    /// <param name="pos">The position of the tile</param>
+    /// <param name="mesh">The tile's mesh</param>
+    /// <param name="material">The tile's material</param>
+    private void InstantiateTiles(string tileInfo, Vector3 pos, Mesh mesh, Material material)
     {
-        float xPos = ((chunkRow - (mapWidth*mapPosition[0])) * tileWidth);
+        Tile tile;
+        if (tileInfo.Length ==3)
+        {
+            Quaternion quat = RotateTile(tileInfo[1].ToString(), tileInfo[2].ToString());
+            tile=Instantiate(_tilePrefab, pos, quat );
+        }
+        else
+        {
+            tile=Instantiate(_tilePrefab, transform);
+        }
+        tile.Init(pos,mesh,material);
+    }
+
+    /// <summary>
+    /// Calculates the tile's position based on it's position in the tile and the
+    /// tile's position in the map
+    /// </summary>
+    /// <param name="height">The height of the tile</param>
+    /// <param name="mapPosition">An array where the chunks in the map</param>
+    /// <param name="tileRow">The row of the tile in chunkPositions</param>
+    /// <param name="tileCol">The col of the tile in chunkPositions</param>
+    /// <returns>Returns the position of the tile in the scene</returns>
+    private Vector3 GetTilePosition(int height, int[] mapPosition, int tileRow, int tileCol)
+    {
+        float xPos = ((tileRow - (mapWidth*mapPosition[0])) * tileWidth);
         float yPos = height-tileHeight-2;
         yPos = Mathf.Clamp(yPos, -tileHeight, 10);
-        float zPos = ((chunkCol - (mapLength*mapPosition[1])) * tileWidth);
+        float zPos = ((tileCol - (mapLength*mapPosition[1])) * tileWidth);
         return new Vector3(xPos, yPos, zPos);
     }
     
@@ -159,6 +176,68 @@ public class ChunkManager : MonoBehaviour
         }
 
         return meshDictionary[TileMesh.Center];
+    }
+
+    private Quaternion RotateTile(string type,string direction)
+    {
+        if (type == "c")
+        {
+            return RotateCorner(direction);
+        }
+        if (type == "l")
+        {
+            Quaternion quat = RotateLine(direction);
+            return quat;
+        }
+
+        if (type == "t")
+        {
+            return RotateMultiplePaths(direction);
+        }
+        return Quaternion.identity;
+    }
+
+    private Quaternion RotateMultiplePaths(string direction)
+    {
+        float y=0;
+        if (direction == "t")
+        {
+            y = 90;
+        }else if (direction == "l")
+        {
+            y = 180;
+        }else if (direction == "d")
+        {
+            y = -90;
+        }
+        return Quaternion.Euler(0,y,0);
+    }
+    
+    private Quaternion RotateLine(string direction)
+    {
+        float y=0;
+        if (direction == "v")
+        {
+            y = 90;
+        }
+        Quaternion quat = Quaternion.Euler(0,y,0);
+        return quat;
+    }
+
+    private Quaternion RotateCorner(string direction)
+    {
+        float y=0;
+        if (direction == "a")
+        {
+            y = -90f;
+        }else if (direction == "c")
+        {
+            y = 90;
+        }else if (direction == "d")
+        {
+            y = 180;
+        }
+        return Quaternion.Euler(0,y,0);
     }
 }
 
