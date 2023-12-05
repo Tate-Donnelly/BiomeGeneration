407b29801d6b16057b94ba68eb5a07eac9041f31
Tate <tate.donne@gmail.com>
2023-12-05 15:06:01 -0500

    Prop generating

--- BiomeGeneration/Assets/Game/Scripts/MapObjects/Chunks/Tile.cs
+++ BiomeGeneration/Assets/Game/Scripts/MapObjects/Chunks/Tile.cs
@@ -6,24 +6,30 @@ public class Tile : MapObject
 {
     //0 is left of the chunk, 1 is above the chunk, 2 is to the right of the chunk, 3 is below the chunk
     private Tile[] nearbyChunks = new Tile[4];
-    private MapObject mapObject;
+    [SerializeField] private MapObject mapObjectPrefab;
+    private Mesh propMesh;
     
     public void Init(Vector3 position, Mesh mesh, Material material)
     {
         base.Init(mesh, material);
         transform.position = position;
-        InstantiateObject();
+    }
+    
+    public void Init(Vector3 position, Mesh mesh, Material material, Mesh propMesh)
+    {
+        base.Init(mesh, material);
+        transform.position = position;
+        this.propMesh = propMesh;
+        InstantiateProp(material);
     }
 
-    private void InstantiateObject()
+    private void InstantiateProp(Material material)
     {
-        if (mapObject != null)
-        {
-            Vector3 position = transform.position;
-            MapObject environmentObject =Instantiate(mapObject);
-            environmentObject.transform.position =
-                new Vector3(position.x, position.y + ChunkManager.tileHeight, position.z);
-        }
+        Vector3 position = transform.position;
+        MapObject environmentObject =Instantiate(mapObjectPrefab);
+        environmentObject.Init(propMesh,material);
+        environmentObject.transform.position =
+            new Vector3(position.x, position.y, position.z);
     }
     
 }
